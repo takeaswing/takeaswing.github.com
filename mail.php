48f97441a4899b871d5c2d6d91481b900c62d25a
Ben Stolovitz <ben@stolovitz.com>
2012-02-18 23:32:24 -0500

    Try GET

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
 
-mail('ben+tas@stolovitz.com', 'Site Response', $_POST['message'], 'From: ' . $_POST['email'] . "\r\n" .
-    'Reply-To: ' . $_POST['email'] . "\r\n" .
+mail('ben+tas@stolovitz.com', 'Site Response', $_GET['message'], 'From: ' . $_GET['email'] . "\r\n" .
+    'Reply-To: ' . $_GET['email'] . "\r\n" .
     'X-Mailer: PHP/' . phpversion()
