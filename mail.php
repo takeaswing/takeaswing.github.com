67e5018487d1ad1b990150f3d127d0e43482b752
Ben Stolovitz <ben@stolovitz.com>
2012-02-18 23:55:14 -0500

    Gave up, link to Wufoo.

--- mail.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-mail('ben+tas@stolovitz.com', 'Site Response', $_POST['message'], 'From: ' . $_POST['email'] . "\r\n" .
-    'Reply-To: ' . $_POST['email'] . "\r\n" .
-    'X-Mailer: PHP/' . phpversion());
