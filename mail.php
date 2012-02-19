8eb7abeddf57c525e524c0e393aa63d522b988be
Ben Stolovitz <ben@stolovitz.com>
2012-02-18 23:30:54 -0500

    Added Ariel Stern and Form Mailer.

---
+++ mail.php
@@ -0,0 +1,5 @@
+<?php
+
+mail('ben+tas@stolovitz.com', 'Site Response', $_POST['message'], 'From: ' . $_POST['email'] . "\r\n" .
+    'Reply-To: ' . $_POST['email'] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion()
